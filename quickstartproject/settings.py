c2406062aef6a24fce79c1c457290292bd561255
ebertonmarinho <eberton.marinho@escolar.ifrn.edu.br>
2022-11-25 10:25:42 -0300

    V 0.2

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rZ76PScTaBk9iuFoq1oCjWXdq5s6TNHgP9xKmfm9NCivjPohmuogHJDkzsd0'
+SECRET_KEY = '1234567890'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://appsupermarket1.azurewebsites.net']
+ALLOWED_HOSTS = []
 
 
 # Application definition
